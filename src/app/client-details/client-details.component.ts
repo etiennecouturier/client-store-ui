9ee952af91022929f08381366c069f5ee0140863
etiennecouturier <iistvan.szabo2@gmail.com>
2021-05-27 03:07:42 +0200

    rearrange details with mat-card

--- src/app/client-details/client-details.component.ts
+++ src/app/client-details/client-details.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Client} from '../model/client';
+import {HttpService} from '../services/http.service';
 
 @Component({
   selector: 'client-details',
@@ -12,10 +13,17 @@ export class ClientDetailsComponent implements OnInit {
   public client: Client;
   edit = false;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute,
+              private httpService: HttpService) {
+  }
 
   ngOnInit(): void {
     this.client = this.route.snapshot.data['client'];
+  }
+
+  save() {
+    this.edit = !this.edit;
+    this.httpService.save('/clients/new', this.client);
   }
 
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {Page} from '../model/page';
 import {environment} from '../../environments/environment';
 
 
@@ -14,32 +13,13 @@ export class HttpService {
     return this.http.get<T>(environment.baseUrl + url, { params : params });
   }
 
-  findAll<T>(url: String, params: any = {}): Observable<Array<T>> {
-    console.log(url);
-    return this.http.get<Array<T>>(environment.baseUrl + url, { params : params });
-  }
-
-  findPage<T>(url,
-              offset,
-              limit,
-              searchFilter,
-              sortBy: string = 'name',
-              sortDirection: string = 'asc'):  Observable<Page<T>> {
-    let params = new HttpParams();
-    params = params.set('page', offset);
-    params = params.set('size', limit);
-    params = params.set('name', searchFilter);
-    params = params.set('sortBy', sortBy);
-    params = params.set('sortDirection', sortDirection);
-    return this.http.get<Page<T>>(environment.baseUrl + url, {params : params});
-  }
-
   deleteById(url: String, id: number): Observable<Object> {
     return this.http.delete(environment.baseUrl + url + id);
   }
 
-  saveProduct<T>(url: String, elem: T) {
-    return this.http.post<T>(environment.baseUrl + url, elem, {});
+  save<T>(url: String, elem: T) {
+    return this.http.post<T>(environment.baseUrl + url, elem, {})
+      .subscribe(resp => console.log(resp));
   }
 
 }
